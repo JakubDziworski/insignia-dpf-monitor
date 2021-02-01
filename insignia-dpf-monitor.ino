9aa3fec1a0d7b99962f715dc97e2a9b26d68e307
jakubdziworski <jakub.dziworski@gmail.com>
2021-02-02 00:57:40 +0100

    bluetooth to usb migration

--- insignia-dpf-monitor.ino
+++ insignia-dpf-monitor.ino
@@ -3,7 +3,6 @@
 #include <SoftwareSerial.h>
 #include "ELMduino.h"
 #include <SPI.h>
-#include <Wire.h>
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
 
@@ -11,28 +10,23 @@ LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars
 ELM327 vgate;
 SoftwareSerial vgateSerial(12, 7);
 
-const long debugRate = 115200;
-
 void setup()
 {
-  Serial.begin(debugRate);
+  Serial.begin(115200);
   Serial.println("START");
-  setupBt();
+  vgateSerial.begin(38400);
+  Serial.println("vgate beginned");
   setupTFT();
   lcd.init();
   lcd.backlight();
+  delay(200);
 }
 
 void loop()
 {
-  if(isBtConfBtnPressed()) {
-    pairWithVgate();
-  }
   if(!vgate.connected) {
-    setup();
-    resetBt();
     connect();
-    delay(1000);
+    delay(500);
     return;
   }
   String voltage = getVoltage();
@@ -56,15 +50,17 @@ void connect() {
   {
     Serial.println("Couldn't connect to OBD scanner");
     lcd.print("VGATE UNCONNECTED");
+    printTFTMessage("SERIAL FAILED");
     return;
   }
   if(vgate.sendCommand("AT SH 7E0") != ELM_SUCCESS) {
     Serial.println("Unable to set header to 7E0");
     lcd.print("UNABLE TO SET AT SH 7E0");
+    printTFTMessage("FAILED 7E0");
     return;
   }
-  setupTFT();
   lcd.print("CONNECTED");
+  printTFTMessage("CONNECTED");
   Serial.println("Connected to OBD scanner");
 }
 
@@ -188,7 +184,7 @@ int32_t handleResponse(const char* command, uint8_t result) {
 //##### TFT SECTION #########
 
 #define TFT_CS        10
-#define TFT_RST        8 // Or set to -1 and connect to Arduino RESET pin
+#define TFT_RST        -1 // Or set to -1 and connect to Arduino RESET pin
 #define TFT_DC         9
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
@@ -196,15 +192,20 @@ Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
 void setupTFT() {
   tft.initR(INITR_BLACKTAB);
-  tft.fillScreen(tft.color565(15,15,15));
   tft.setRotation(1);
   tft.setTextWrap(false);
+  clearTft();
+}
+
+void clearTft() {
+  tft.fillScreen(tft.color565(15,15,15));
 }
 
 
 void printTFTMessage(const char* command) {
     int16_t  tx, ty;
     uint16_t tw, th;
+    clearTft();
     tft.setTextSize(2);
     tft.getTextBounds(command, tft.width()/2, tft.height()/2, &tx, &ty, &tw, &th);
     tft.setTextColor(ST77XX_WHITE);
@@ -234,95 +235,4 @@ void drawProgressBar(float value, float maxValue, int x, int y, int w, int h, co
 void printOilPressure(int pressure, int rpm, bool is_critical) {
   drawProgressBar(pressure, 500, 5,5,tft.width() - 10, tft.height()/2-10, "KPA", tft.color565(0, 168, 87),0);
   drawProgressBar((float)pressure/(float)rpm, 0.5f, 5, tft.height()/2+5, tft.width() - 10, tft.height()/2-10, "KPA/RPM", tft.color565(0, 140, 168),2);
-}
-
-//##### BLUETOOTH AUTOCONFIGURATION SECTION #########
-const int btConfPin = 3 ;
-const int btConfBtn = 4;
-const int btPowerPin = 5;
-const long vgateRate = 9600;
-const long confRate = 38400;
-
-void setupBt() {
-  vgateSerial.begin(vgateRate);
-  pinMode(btPowerPin, OUTPUT);
-  pinMode(btConfPin, OUTPUT);
-  pinMode(btConfBtn, INPUT_PULLUP);
-  digitalWrite(btPowerPin, HIGH);
-  digitalWrite(btConfPin, LOW);
-}
-
-void resetBt() {
-  digitalWrite(btPowerPin, LOW);
-  delay(500);
-  digitalWrite(btPowerPin, HIGH);
-  delay(500);
-}
-
-boolean isBtConfBtnPressed() {
-  Serial.println(digitalRead(btConfBtn));
-  return digitalRead(btConfBtn) == LOW;
-}
-
-//##### AUTOCONFIGURATION SECTION #########
-void pairWithVgate()
-{
-    lcd.clear();
-    vgateSerial.end();
-    vgateSerial.begin(confRate);
-
-    digitalWrite(btPowerPin, LOW);
-    digitalWrite(btConfPin, HIGH);
-    delay(1000);
-    digitalWrite(btPowerPin, HIGH);
-    delay(4000);
-
-    sendBtConfCommand("AT+RMAAD");
-    sendBtConfCommand("AT+ORGL");
-    sendBtConfCommand("AT+ROLE=1");
-    sendBtConfCommand("AT+RESET",4000);
-    sendBtConfCommand("AT+CMODE=0");
-    sendBtConfCommand("AT+BIND=86DC,3D,ABF7F1");
-    sendBtConfCommand("AT+INIT");
-    sendBtConfCommand("AT+PAIR=86DC,3D,ABF7F1,20",10000L);
-    sendBtConfCommand("AT+LINK=86DC,3D,ABF7F1",10000L);
-    Serial.println("Done");
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("BLUETOOTH CONF");
-    lcd.setCursor(0,1);
-    lcd.print("DONE");
-    delay(3000);
-    vgateSerial.end();
-    vgateSerial.begin(confRate);
-
-    digitalWrite(btPowerPin, LOW);
-    digitalWrite(btConfPin, LOW);
-    delay(1000);
-    digitalWrite(btPowerPin, HIGH);
-    lcd.clear();
-}
-
-void sendBtConfCommand(char* cmd) {
-  sendBtConfCommand(cmd, 2000);
-}
-
-void sendBtConfCommand(char* cmd, long timeout) {
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print(cmd);
-  lcd.setCursor(0,1);
-  Serial.println(cmd);
-  long start = millis();
-  vgateSerial.println(cmd);
-
-  while((millis() - start) < timeout) {
-    if (vgateSerial.available()) {
-      char c = vgateSerial.read();
-      lcd.print(c);
-      Serial.write(c);
-    }
-    delay(5);
-  }
-  lcd.clear();
 }
